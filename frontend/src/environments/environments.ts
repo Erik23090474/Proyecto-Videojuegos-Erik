23d7cfcc9211f558b288592792cd3ec8f2acce08
Brandon Diaz <utm23090474@utma.edu.mx>
2026-03-19 21:13:12 -0600

    Conexión de Angular con la API de Railway

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -9,5 +9,5 @@ export const environment = {
     appId: "1:844466379239:web:a0b740308dff49c5808542"
   },
   // Esto lo usaremos cuando hagamos el backend con Node.js
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'proyecto-videojuegos-erik-production.up.railway.app'
 };
